5ca3dadee10740433f963a88da275d6967094de4
mdmoreau <mdmoreau@gmail.com>
2014-11-20 20:55:23 -0500

    header, footer, about template, gulpfile fixes, main nav liquid fix

--- gulpfile.js
+++ gulpfile.js
@@ -49,14 +49,14 @@ gulp.task('sass', function () {
 // Scripts
 gulp.task('scripts', function() {
   return gulp.src('app/js/**/*.js')
-    .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('default'))
+    //.pipe(jshint('.jshintrc'))
+    //.pipe(jshint.reporter('default'))
     .pipe(concat('app.js'))
     .pipe(gulp.dest('assets/scripts'))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(browserify())
-    .pipe(uglify())
-    .pipe(gulp.dest('assets/scripts'))
+    //.pipe(rename({ suffix: '.min' }))
+    //.pipe(browserify())
+    //.pipe(uglify())
+    //.pipe(gulp.dest('assets/scripts'))
 });
 
 
@@ -65,5 +65,5 @@ gulp.task('scripts', function() {
 gulp.task('default', ['sass', 'browser-sync'], function () {
   gulp.watch("app/scss/**/*.scss", ['sass']);
   gulp.watch("**/*.html", browserSync.reload);
-  gulp.watch("app/js/*.js", ['js', browserSync.reload]);
+  gulp.watch("app/js/*.js", ['scripts', browserSync.reload]);
 });
